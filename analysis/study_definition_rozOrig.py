0837c4f8c89938fb1132f48df561dd638c087f77
Kev Wing <64849534+kevwing@users.noreply.github.com>
2020-08-12 14:52:00 +0100

    Original codelists selected by Roz

---
+++ analysis/study_definition_rozOrig.py
@@ -0,0 +1,361 @@
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+)
+
+# modified from the risk-prediction-research study definition
+
+## CODE LISTS
+# All codelist are held within the codelist/ folder.
+from codelists import *
+
+## STUDY POPULATION
+# Defines both the study population and points to the important covariates
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1970-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.2,
+    },
+    # This line defines the study population
+    population=patients.registered_with_one_practice_between(
+        "2019-02-01", "2020-02-01"
+    ),
+    # Outcomes
+    icu_date_admitted=patients.admitted_to_icu(
+        on_or_after="2020-02-01",
+        include_day=True,
+        returning="date_admitted",
+        find_first_match_in_period=True,
+    ),
+    died_date_cpns=patients.with_death_recorded_in_cpns(
+        on_or_before="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+    ),
+    died_ons_covid_flag_any=patients.with_these_codes_on_death_certificate(
+        covid_codelist,
+        on_or_before="2020-06-01",
+        match_only_underlying_cause=False,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_ons_covid_flag_underlying=patients.with_these_codes_on_death_certificate(
+        covid_codelist,
+        on_or_before="2020-06-01",
+        match_only_underlying_cause=True,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_date_ons=patients.died_from_any_cause(
+        on_or_before="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+    ),
+    # The rest of the lines define the covariates with associated GitHub issues
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/33
+    age=patients.age_as_of(
+        "2020-02-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/46
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
+    imd=patients.address_as_of(
+        "2020-02-01",
+        returning="index_of_multiple_deprivation",
+        round_to_nearest=100,
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
+        },
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/37
+    rural_urban=patients.address_as_of(
+        "2020-02-01",
+        returning="rural_urban_classification",
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"rural": 0.1, "urban": 0.9}},
+        },
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
+    stp=patients.registered_practice_as_of(
+        "2020-02-01",
+        returning="stp_code",
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "STP1": 0.1,
+                    "STP2": 0.1,
+                    "STP3": 0.1,
+                    "STP4": 0.1,
+                    "STP5": 0.1,
+                    "STP6": 0.1,
+                    "STP7": 0.1,
+                    "STP8": 0.1,
+                    "STP9": 0.1,
+                    "STP10": 0.1,
+                }
+            },
+        },
+    ),
+    # region - one of NHS England 9 regions
+    region=patients.registered_practice_as_of(
+        "2020-02-01",
+        returning="nuts1_region_name",
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "North East": 0.1,
+                    "North West": 0.1,
+                    "Yorkshire and the Humber": 0.1,
+                    "East Midlands": 0.1,
+                    "West Midlands": 0.1,
+                    "East of England": 0.1,
+                    "London": 0.2,
+                    "South East": 0.2,
+                },
+            },
+        },
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/10
+    bmi=patients.most_recent_bmi(
+        on_or_after="2010-02-01",
+        minimum_age_at_measurement=16,
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 35, "stddev": 10},
+        },
+    ),
+    ethnicity_16=patients.with_these_clinical_events(
+        ethnicity_codes_16,
+        returning="category",
+        find_last_match_in_period=True,
+        return_expectations={
+            "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
+        },
+    ),
+    primary_care_case=patients.with_these_clinical_events(
+        covid_primary_care_case,
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 20.0, "stddev": 10},
+            "date": {"earliest": "2019-02-28", "latest": "2020-02-29"},
+            "incidence": 0.15,
+        },
+    ),
+    primary_care_historic_case=patients.with_these_clinical_events(
+        covid_primary_care_historic_case,
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 20.0, "stddev": 10},
+            "date": {"earliest": "2019-02-28", "latest": "2020-02-29"},
+            "incidence": 0.1,
+        },
+    ),
+    primary_care_potential_historic_case=patients.with_these_clinical_events(
+        covid_primary_care_potential_historic_case,
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 20.0, "stddev": 10},
+            "date": {"earliest": "2019-02-28", "latest": "2020-02-29"},
+            "incidence": 0.05,
+        },
+    ),
+    primary_care_exposure=patients.with_these_clinical_events(
+        covid_primary_exposure,
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 20.0, "stddev": 10},
+            "date": {"earliest": "2019-02-28", "latest": "2020-02-29"},
+            "incidence": 0.15,
+        },
+    ),
+    primary_care_suspect_case=patients.with_these_clinical_events(
+        covid_primary_care_suspect_case,
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 20.0, "stddev": 10},
+            "date": {"earliest": "2019-02-28", "latest": "2020-02-29"},
+            "incidence": 0.35,
+        },
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/21
+    chronic_respiratory_disease=patients.with_these_clinical_events(
+        chronic_respiratory_disease_codes,
+        return_first_date_in_period=True,
+        include_month=True,
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/55
+    asthma=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "1": """
+                (
+                  recent_asthma_code OR (
+                    asthma_code_ever AND NOT
+                    copd_code_ever
+                  )
+                ) AND (
+                  prednisolone_last_year = 0 OR 
+                  prednisolone_last_year > 4
+                )
+            """,
+            "2": """
+                (
+                  recent_asthma_code OR (
+                    asthma_code_ever AND NOT
+                    copd_code_ever
+                  )
+                ) AND
+                prednisolone_last_year > 0 AND
+                prednisolone_last_year < 5
+                
+            """,
+        },
+        return_expectations={"category": {"ratios": {"0": 0.8, "1": 0.1, "2": 0.1}},},
+        recent_asthma_code=patients.with_these_clinical_events(
+            asthma_codes, between=["2017-02-01", "2020-02-01"],
+        ),
+        asthma_code_ever=patients.with_these_clinical_events(asthma_codes),
+        copd_code_ever=patients.with_these_clinical_events(
+            chronic_respiratory_disease_codes
+        ),
+        prednisolone_last_year=patients.with_these_medications(
+            pred_codes,
+            between=["2019-02-01", "2020-02-01"],
+            returning="number_of_matches_in_period",
+        ),
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/7
+    chronic_cardiac_disease=patients.with_these_clinical_events(
+        chronic_cardiac_disease_codes,
+        return_first_date_in_period=True,
+        include_month=True,
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/30
+    diabetes=patients.with_these_clinical_events(
+        diabetes_codes, return_first_date_in_period=True, include_month=True,
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/32
+    lung_cancer=patients.with_these_clinical_events(
+        lung_cancer_codes, return_first_date_in_period=True, include_month=True,
+    ),
+    haem_cancer=patients.with_these_clinical_events(
+        haem_cancer_codes, return_first_date_in_period=True, include_month=True,
+    ),
+    other_cancer=patients.with_these_clinical_events(
+        other_cancer_codes, return_first_date_in_period=True, include_month=True,
+    ),
+    # # https://github.com/ebmdatalab/tpp-sql-notebook/issues/12
+    chronic_liver_disease=patients.with_these_clinical_events(
+        chronic_liver_disease_codes,
+        return_first_date_in_period=True,
+        include_month=True,
+    ),
+    # # https://github.com/ebmdatalab/tpp-sql-notebook/issues/14
+    other_neuro=patients.with_these_clinical_events(
+        other_neuro, return_first_date_in_period=True, include_month=True,
+    ),
+    stroke=patients.with_these_clinical_events(
+        stroke, return_first_date_in_period=True, include_month=True,
+    ),
+    dementia=patients.with_these_clinical_events(
+        dementia, return_first_date_in_period=True, include_month=True,
+    ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/23
+    # immunosuppressant_med=
+    # hypertension
+    hypertension=patients.with_these_clinical_events(
+        hypertension_codes, return_first_date_in_period=True, include_month=True,
+    ),
+    positive_covid_test_ever=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        return_expectations={"incidence": 0.1},
+    ),
+    negative_covid_test_ever=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="negative",
+        return_expectations={"incidence": 0.9},
+    ),
+    tested_for_covid=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="any",
+        on_or_before="2020-05-01",
+        return_expectations={"incidence": 0.25},
+    ),
+    first_positive_test_date=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        find_first_match_in_period=True,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 60.0, "stddev": 15},
+            "date": {"earliest": "2020-03-28", "latest": "2020-06-22"},
+            "incidence": 0.1,
+        },
+    ),
+    care_home_type=patients.care_home_status_as_of(
+        "2020-02-01",
+        categorised_as={
+            "PC": """
+              IsPotentialCareHome
+              AND LocationDoesNotRequireNursing='Y'
+              AND LocationRequiresNursing='N'
+            """,
+            "PN": """
+              IsPotentialCareHome
+              AND LocationDoesNotRequireNursing='N'
+              AND LocationRequiresNursing='Y'
+            """,
+            "PS": "IsPotentialCareHome",
+            "U": "DEFAULT",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"PC": 0.01, "PN": 0.01, "PS": 0.01, "U": 0.97,},},
+        },
+    ),
+    household_id=patients.household_as_of(
+        "2020-02-01",
+        returning="pseudo_id",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 1000, "stddev": 200},
+            "incidence": 1,
+        },
+    ),
+    household_size=patients.household_as_of(
+        "2020-02-01",
+        returning="household_size",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 1.5},
+            "incidence": 1,
+        },
+    ),
+)
